fcb90d84234a0fdba2cef7b586fba8ff40946776
Jeffrey Jaca <jeffreyjjaca@gmail.com>
2020-10-01 19:08:35 -0400

    added RPS logic
    - placed radio buttons within a radio button group
    - added functionality to play button in InputActivity
    - added variables to hold UI references in both activities

--- app/src/main/java/sheridan/jaca/assignment1/InputActivity.kt
+++ app/src/main/java/sheridan/jaca/assignment1/InputActivity.kt
@@ -1,11 +1,29 @@
 package sheridan.jaca.assignment1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import sheridan.jaca.assignment1.databinding.ActivityMainBinding
 
 class InputActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var playerChoice: String
+    private lateinit var computerChoice: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = binding.root
+        setContentView(rootView)
+
+        val btnPlay = binding.btnPlay
+        btnPlay.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, OutputActivity::class.java)
+            intent.putExtra("playerChoice",playerChoice)
+            intent.putExtra("computerChoice",computerChoice)
+            startActivity(intent)
+        })
     }
+
 }
--- app/src/main/java/sheridan/jaca/assignment1/OutputActivity.kt
+++ app/src/main/java/sheridan/jaca/assignment1/OutputActivity.kt
@@ -2,10 +2,62 @@ package sheridan.jaca.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sheridan.jaca.assignment1.databinding.ActivityOutputBinding
 
 class OutputActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOutputBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_output)
+        binding = ActivityOutputBinding.inflate(layoutInflater)
+        var rootView = binding.root
+        setContentView(rootView)
+
+        val player = intent.getStringExtra("playerChoice")
+        val computer = intent.getStringExtra("computerChoice")
+        val txtPlayerChoice = binding.txtPlayerChoice
+        val txtComputerChoice = binding.txtComputerChoice
+        val txtWinner = binding.txtWinner
+        val btnReplay = binding.btnReplay
+
+        txtPlayerChoice.text = player
+        txtComputerChoice.text = computer
+
+        val result = determineResult(player,computer)
+
+        txtWinner.text = result
+    }
+
+    private fun determineResult(player: String?, computer:String?): String{
+        var result:String = ""
+        if(player=="Rock"){
+            if (computer=="Rock"){
+                result = "It's a Draw!!!"
+            }else if (computer == "Paper"){
+                result = "The Computer!!!"
+            }else if (computer == "Scissors"){
+                result = "The Player!!!"
+            }
+        }else if (player == "Paper"){
+            if (computer=="Rock"){
+                result = "The Player!!!"
+            }else if (computer == "Paper"){
+                result = "It's a Draw!!!"
+            }else if (computer == "Scissors"){
+                result = "The Computer!!!"
+            }
+        }else if (player == "Scissors"){
+            if (computer=="Rock"){
+                result = "The Computer!!!"
+            }else if (computer == "Paper"){
+                result = "The Player!!!"
+            }else if (computer == "Scissors"){
+                result = "It's a Draw!!!"
+            }
+        }else {
+            result = "WHAT something went wrong!!!"
+        }
+
+        return result
     }
 }
